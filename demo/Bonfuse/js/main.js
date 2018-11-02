930524ac13a69d671eba4788104d1f2e192a79e2
Alexey Grevtsov <grevtsov.web@gmail.com>
2018-11-03 00:46:09 +0300

    main-page commit

--- demo/Bonfuse/js/main.js
+++ demo/Bonfuse/js/main.js
@@ -42,5 +42,51 @@ $(function(){
 	});
 
 
+	var section = $('.section-js'),
+		balloon  = $('.balloon-js'),
+		balloon2 = $('.balloon-2-js'),
+		balloon3 = $('.balloon-3-js');
+
+
+
+	console.log();
+
+	$(window).on('scroll', function(){
+
+		var sectionOffset = $(window).scrollTop(),
+			sectionHeight = section.height();
+
+		/*balloon2.css('transform', 'scale(' + (1 - sectionOffset / 3000) + ')' + 'translateY(' + (-sectionOffset / 4) + 'px' + ')');
+
+		balloon3.css('transform', 'scale(' + (1 - sectionOffset / 3000) + ')' + 'translateY(' + (-sectionOffset / 4) + 'px' + ')');*/
+	
+		if( $(window).scrollTop() > sectionHeight - 200){
+
+			balloon2.css('transform', 'translate(' + -200 + 'px' +',' + -1000 + 'px' + ')');
+			balloon3.css('transform', 'translate(' + 200 + 'px' +',' + -1000 + 'px' + ')');
+			balloon.removeClass('balloon-move');
+			setTimeout(function(){
+				balloon.addClass('hide-balloon')
+			}, 20);
+			
+
+			/*balloon.addClass('hide-baloon');
+			balloon2.addClass('hide-baloon');
+			balloon3.addClass('hide-baloon');*/
+			
+		}else{
+			balloon2.css('transform', 'scale(' + (1 - sectionOffset / 3000) + ')' + 'translateY(' + (-sectionOffset / 4) + 'px' + ')');
+			balloon3.css('transform', 'scale(' + (1 - sectionOffset / 3000) + ')' + 'translateY(' + (-sectionOffset / 4) + 'px' + ')');
+			balloon.removeClass('hide-balloon');
+		
+			
+			/*balloon.removeClass('hide-baloon');
+			balloon2.removeClass('hide-baloon');
+			balloon3.removeClass('hide-baloon');*/
+		}
+	});
+
 
 });
+
+new WOW().init();
