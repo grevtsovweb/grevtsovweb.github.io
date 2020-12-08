b2dbb63b9f29c9b46f69773277a5506956f054fb
grevtsovweb <grevtsov.web@gmail.com>
2020-12-08 11:44:14 +0300

    Added close button

--- demo/Mathlab/js/main.js
+++ demo/Mathlab/js/main.js
@@ -3,12 +3,17 @@ document.addEventListener('DOMContentLoaded', function(){
     if(window.innerWidth < 993){
         let openMenuButton = document.querySelector('.mobile-menu-button-js');
         let mobileMenu = document.querySelector('.mobile-menu-js');
+        let closeMenuButton = document.querySelector('.mobile-menu-close-button-js');
 
         openMenuButton.addEventListener('click', function(e){
             e.preventDefault();
-            console.log(1)
-            mobileMenu.classList.add('mobile-menu_active')
-        })
+            mobileMenu.classList.add('mobile-menu_active');
+
+            closeMenuButton.addEventListener('click', function(e){
+                e.preventDefault();
+                mobileMenu.classList.remove('mobile-menu_active');
+            });
+        });
     }
 
 });
